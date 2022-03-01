39a635ee398d89c04286e219028651ee47ad50c3
jessicabetonico <jessicabetonico@gmail.com>
2022-03-01 16:35:57 -0300

    feat: add test to verify styles color

---
+++ src/components/Main/__test__/test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '..'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react with NextJs - boilerplate/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
+})
