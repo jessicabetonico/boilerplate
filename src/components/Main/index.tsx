ef9ca8f24cb7e35b10f13ff2b83800a57d65099c
jessicabetonico <jessicabetonico@gmail.com>
2022-03-01 16:32:44 -0300

    feat: use new tags with styled

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,16 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Image of an atom, react symbol, next to a text 'React with NextJS - Boilerplate'"
+    />
+    <S.Title>React with NextJs - Boilerplate</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS and Styled Component
+    </S.Description>
+  </S.Wrapper>
+)
+
+export default Main
